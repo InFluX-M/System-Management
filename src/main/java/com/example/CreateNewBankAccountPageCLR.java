014bede2fc789b9cb5a5bf5ac21e65c5a7b2a6bd
matin-azami <matin.azami.03@gmail.com>
2022-06-26 19:41:55 +0430

    Debug

---
+++ src/main/java/com/example/CreateNewBankAccountPageCLR.java
@@ -0,0 +1,111 @@
+package com.example;
+
+import com.example.finalpr.Availabilities.BankInterestPercentage;
+import com.example.finalpr.Availabilities.CurrentAccount;
+import com.example.finalpr.Availabilities.GoodLoanAccount;
+import com.example.finalpr.Availabilities.SavingAccount;
+import com.example.finalpr.MYSQL.CurrentBankAccounts;
+import com.example.finalpr.MYSQL.SavingBankAccounts;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+import static com.example.finalpr.HelloApplication.bankSystem;
+
+public class CreateNewBankAccountPageCLR implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        KindBankAccount.getItems().addAll("CurrentAccount", "SavingAccount", "GoodLoanAccount");
+        KindBankInterestPercentage.getItems().addAll("SHORT_TERM", "LONG_TERM", "SPECIAL");
+    }
+
+    @FXML
+    private Label AccountNumber;
+
+    @FXML
+    private JFXComboBox<String> KindBankAccount;
+
+    @FXML
+    private JFXComboBox<String> KindBankInterestPercentage;
+
+    @FXML
+    private TextField OwnerID;
+
+    @FXML
+    private Label Register;
+
+    @FXML
+    void Register(MouseEvent event) {
+
+        String accountNumber = bankSystem.getAccountNumber();
+        accountNumber = "5";
+        String ownerID = OwnerID.getText();
+        double balance = 0.0;
+        LocalDate dateCreate = LocalDate.now();
+        int point = 0;
+        String kind = KindBankAccount.getValue();
+
+        if(ownerID.isEmpty() || kind.isEmpty()){
+            Register.setText("Input The Required Fields... :(");
+        }else{
+            if(kind.equals("CurrentAccount")){
+
+                CurrentAccount currentAccount = new CurrentAccount(accountNumber, ownerID, balance, dateCreate, point, null, null);
+                bankSystem.addCurrentBankAccount(currentAccount);
+            }
+            else if(kind.equals("SavingAccount")){
+
+                BankInterestPercentage kindBankInterestPercentage = null;
+                if(KindBankInterestPercentage.getValue().equals("SHORT_TERM")){
+                    kindBankInterestPercentage = BankInterestPercentage.SHORT_TERM;
+                }else if(KindBankInterestPercentage.getValue().equals("LONG_TERM")){
+                    kindBankInterestPercentage = BankInterestPercentage.LONG_TERM;
+                }else if(KindBankInterestPercentage.getValue().equals("SPECIAL")){
+                    kindBankInterestPercentage = BankInterestPercentage.SPECIAL;
+                }
+
+                SavingAccount savingAccount = new SavingAccount(accountNumber, ownerID, balance, dateCreate, point, kindBankInterestPercentage);
+                bankSystem.addSavingAccount(savingAccount);
+            }
+            else if(kind.equals("GoodLoanAccount")){
+
+                GoodLoanAccount goodLoanAccount = new GoodLoanAccount(accountNumber, ownerID, balance, dateCreate, point, null);
+                bankSystem.addGoodLoanAccount(goodLoanAccount);
+            }
+        }
+
+        Register.setText("BankAccount Registered Successfully... :)");
+
+    }
+
+    @FXML
+    void SignIn(MouseEvent event) {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+            Stage s1 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            s1.setScene(scene);
+            s1.show();
+        }
+        catch (IOException e) {
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/finalpr/LoginPageCLR.java
@@ -0,0 +1,99 @@
+package com.example.finalpr;
+
+import com.example.finalpr.Systems.BankSystem;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import static com.example.finalpr.HelloApplication.bankSystem;
+
+public class LoginPageCLR implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboRules.getItems().addAll("Admin", "User");
+    }
+
+    @FXML
+    private Circle circle1;
+
+    @FXML
+    private JFXComboBox<String> comboRules;
+
+    @FXML
+    private TextField inputAccountNumber;
+
+    @FXML
+    private PasswordField inputOwnerID;
+
+    @FXML
+    private Label statueLogin;
+
+    @FXML
+    void createNewProfile(MouseEvent event) {
+        System.out.println("Hi");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("CreateNewBankAccountPage.fxml"));
+            Stage s1 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            s1.setScene(scene);
+            s1.show();
+        }
+        catch (IOException e) {
+
+        }
+
+    }
+
+    @FXML
+    void login(MouseEvent event) throws InterruptedException {
+
+        String accountNumber = inputAccountNumber.getText();
+        String ownerID = inputOwnerID.getText();
+        String rule = comboRules.getValue();
+
+        if(accountNumber.isEmpty() || ownerID.isEmpty() || rule.isEmpty()){
+            statueLogin.setText("Input The Required Fields... :(");
+        }else{
+
+            if(rule.equals("Admin")){
+
+                if(accountNumber.equals("admin") && ownerID.equals("admin")){
+
+
+                }else{
+                    statueLogin.setText("Account Is Not Valid... :(");
+                }
+
+            }else if(rule.equals("User")){
+
+                if(bankSystem.loginBankAccount(accountNumber, ownerID)){
+
+
+                }else{
+                    statueLogin.setText("Account Is Not Valid... :(");
+                }
+
+            }else{
+                statueLogin.setText("Input Invalid Rule... :(");
+            }
+        }
+
+    }
+
+}
